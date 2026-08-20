ebc70bc41898da467b4c8639df596000fe48212e
Sofi <sofiia.velykokhatska@gmail.com>
2026-08-20 15:02:16 +0200

    Display fake tickets in Material table

--- ticket-manager/src/app/ticket-list/ticket-list.ts
+++ ticket-manager/src/app/ticket-list/ticket-list.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { TicketDataService } from '../ticket-data';
 import { Ticket } from '../ticket';
 
 @Component({
   selector: 'app-ticket-list',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './ticket-list.html',
   styleUrl: './ticket-list.css'
 })
+
 export class TicketList {
   tickets: Ticket[] = [];
+  displayedColumns: string[] = ['subject', 'status', 'priority', 'createdAt'];
 
   constructor(private ticketData: TicketDataService) {
     this.tickets = this.ticketData.getTickets();
